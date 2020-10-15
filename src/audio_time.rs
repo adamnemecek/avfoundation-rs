179e037633f3f8e690ee96bf291abd186c2bf52a
Adam Nemecek <adamnemecek@gmail.com>
2020-10-14 17:14:10 -0700

    stuff

--- src/audio_time.rs
+++ src/audio_time.rs
@@ -1,5 +1,5 @@
 // use crate::{AudioFormat, AudioNodeBus};
-
+use objc::runtime::{NO, YES};
 pub enum AVAudioTime {}
 
 foreign_obj_type! {
@@ -8,4 +8,41 @@ foreign_obj_type! {
     pub struct AudioTimeRef;
 }
 
-impl AudioTimeRef {}
+impl AudioTimeRef {
+    pub fn is_host_time_valid(&self) -> bool {
+        unsafe {
+            match msg_send![self, isHostTimeValid] {
+                YES => true,
+                NO => false,
+                _ => unreachable!(),
+            }
+        }
+    }
+
+    pub fn host_time(&self) -> u64 {
+        unsafe { msg_send![self, hostTime] }
+    }
+
+    pub fn is_sample_time_valid(&self) -> bool {
+        unsafe {
+            match msg_send![self, isSampleTimeValid] {
+                YES => true,
+                NO => false,
+                _ => unreachable!(),
+            }
+        }
+    }
+
+    // pub fn sampleTime(&self) -> AVAudioFramePosition {
+    //     unsafe { msg_send![self, sampleTime] }
+    //  }
+
+    pub fn sample_rate(&self) -> f64 {
+        unsafe { msg_send![self, sampleRate] }
+    }
+
+    // pub fn audioTimeStamp(&self) -> AudioTimeStamp {
+    //     unsafe { msg_send![self, audioTimeStamp] }
+    //  }
+
+}
