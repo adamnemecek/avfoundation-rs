0da2112d82e35492aa11fea8819a4206d7c523c0
Adam Nemecek <adamnemecek@gmail.com>
2020-11-26 10:44:32 -0800

    audio buffers

--- src/av_audio_buffer.rs
+++ src/av_audio_buffer.rs
@@ -1,7 +1,37 @@
+use crate::{AVAudioFormat};
+pub enum AVAudioBufferFFI {}
+
+foreign_obj_type! {
+    type CType = AVAudioBufferFFI;
+    pub struct AVAudioBuffer;
+    pub struct AVAudioBufferRef;
+}
+
+impl AVAudioBufferRef {
+    pub fn format(&self) -> AVAudioFormat {
+        unsafe { msg_send![self, format] }
+    }
+    // audiobuferlist
+    // mutableaudiobufferlist
+}
+
+
 pub enum AVAudioPCMBufferFFI {}
 
 foreign_obj_type! {
     type CType = AVAudioPCMBufferFFI;
     pub struct AVAudioPCMBuffer;
     pub struct AVAudioPCMBufferRef;
+    type ParentType = AVAudioBufferRef;
 }
+
+
+pub enum AVAudioCompressedBufferFFI {}
+
+foreign_obj_type! {
+    type CType = AVAudioCompressedBufferFFI;
+    pub struct AVAudioCompressedBuffer;
+    pub struct AVAudioCompressedBufferRef;
+    type ParentType = AVAudioBufferRef;
+}
+
