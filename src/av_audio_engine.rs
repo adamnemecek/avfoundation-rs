bf542609aead9bba00914b7b654037ed7d73e783
Adam Nemecek <adamnemecek@gmail.com>
2020-11-29 09:04:39 -0800

    audioengine

--- src/av_audio_engine.rs
+++ src/av_audio_engine.rs
@@ -22,18 +22,124 @@ use objc::runtime::{
 ///
 ///         AVAudioEngineManualRenderingErrorNotRunning
 ///            The operation cannot be performed because the engine is not running (i.e. not started).
+// osstatus
+// API_AVAILABLE(macos(10.13), ios(11.0), watchos(4.0), tvos(11.0));
+pub enum AVAudioEngineManualRenderingError {
+    InvalidMode = -80800,
+    Initialized = -80801,
+    NotRunning = -80802,
+}
 
-// typedef NS_ENUM(OSStatus, AVAudioEngineManualRenderingError) {
-//     AVAudioEngineManualRenderingErrorInvalidMode = -80800,
-//     AVAudioEngineManualRenderingErrorInitialized = -80801,
-//     AVAudioEngineManualRenderingErrorNotRunning  = -80802
-// }  API_AVAILABLE(macos(10.13), ios(11.0), watchos(4.0), tvos(11.0));
+///    @enum AVAudioEngineManualRenderingStatus
+///    @abstract
+///        Status codes returned from the render call to the engine operating in manual rendering mode.
+///
+///        AVAudioEngineManualRenderingStatusError
+///            An error occurred when rendering and no data was returned. See the returned error code
+///            for the description of the error.
+///
+///        AVAudioEngineManualRenderingStatusSuccess
+///            All of the requested data was returned successfully.
+///
+///        AVAudioEngineManualRenderingStatusInsufficientDataFromInputNode
+///            Applicable only to the input node, when it provides input data for rendering
+///            (see `AVAudioInputNode(setManualRenderingInputPCMFormat:inputBlock:)`).
+///            Indicates that not enough input data was returned by the input node to satisfy the
+///            render request at the current time. The output buffer may contain data rendered by other
+///            active sources in the engine's processing graph.
+///
+///         AVAudioEngineManualRenderingStatusCannotDoInCurrentContext
+///            The operation could not be performed now, but the client could retry later if needed.
+///            This is usually to guard a realtime render operation (e.g. rendering through
+///            `manualRenderingBlock`) when a reconfiguration of the engine's internal state
+///            is in progress.
 
+// osstatus
+// API_AVAILABLE(macos(10.13), ios(11.0), watchos(4.0), tvos(11.0));
+pub enum AVAudioEngineManualRenderingStatus {
+    Error = -1,
+    Success = 0,
+    InsufficientDataFromInputNode = 1,
+    CannotDoInCurrentContext = 2,
+}
+
+///    @enum AVAudioEngineManualRenderingMode
+///    @abstract
+///        By default, the engine is connected to an audio device and automatically renders in realtime.
+///        It can also be configured to operate in manual rendering mode, i.e. not connected to an
+///        audio device and rendering in response to requests from the client.
+///
+///        AVAudioEngineManualRenderingModeOffline
+///            The engine operates in an offline mode without any realtime constraints.
+///
+///        AVAudioEngineManualRenderingModeRealtime
+///            The engine operates under realtime constraints, i.e. it will not make any blocking call
+///            (e.g. calling libdispatch, blocking on a mutex, allocating memory etc.) while rendering.
+///            Note that only the block based render mechanism can be used in this mode
+///            (see `AVAudioEngine(manualRenderingBlock)`.
+
+#[repr(i64)]
 pub enum AudioEngineManualRenderingMode {
     Offline,
     RealTime,
 }
 
+///    @typedef AVAudioEngineManualRenderingBlock
+///    @abstract
+///        Block to render the engine when operating in manual rendering mode
+///    @param numberOfFrames
+///        The number of PCM sample frames to be rendered
+///    @param outBuffer
+///        The PCM buffer to which the engine must render the audio.
+///        The buffer pointers (outBuffer->mBuffers[x].mData) may be null on entry, in which case
+///        the block will render into a memory it owns and modify the mData pointers to point to that
+///        memory. The block is responsible for preserving the validity of that memory until it is next
+///        called to render, or `AVAudioEngine(stop)` is called.
+///    @param outError
+///        On exit, if an error occurs during rendering, a description of the error (see
+///        `AVAudioEngineManualRenderingError` for the possible errors)
+///    @return
+///        One of the status codes from `AVAudioEngineManualRenderingStatus`. Irrespective of the
+///        returned status code, on exit, the output buffer's mDataByteSize
+///        (outBuffer->mBuffers[x].mDataByteSize) will indicate the number of PCM data bytes rendered by
+///        the engine.
+///
+///    Use this if you want to render the engine from a realtime context when it is operating in
+///    the manual rendering mode. See `AVAudioEngine(manualRenderingBlock)` for details.
+///
+///    Note that when using AVAudioEngine manual rendering with
+///    `AVAudioEngineManualRenderingModeRealtime`, calling into the engine or related classes
+///    from a non-realtime thread (e.g. for setting or getting node properties), can cause
+///    `AVAudioEngineManualRenderingBlock` on the IO thread to return
+///    `AVAudioEngineManualRenderingStatusCannotDoInCurrentContext`.
+///
+///    This is because interacting with some of these properties requires synchronization between
+///    the realtime and calling threads.
+///
+///    In such a case, the client could implement their own synchronization between their realtime
+///    and non-realtime threads and retry calling `AVAudioEngineManualRenderingBlock`.
+
+// API_AVAILABLE(macos(10.13), ios(11.0), watchos(4.0), tvos(11.0));
+pub type AVAudioEngineManualRenderingBlock = block::Block<(), ()>;
+
+///    @class AVAudioEngine
+///    
+///    An AVAudioEngine contains a group of connected AVAudioNodes ("nodes"), each of which performs
+///    an audio signal generation, processing, or input/output task.
+///    
+///    Nodes are created separately and attached to the engine.
+///    
+///    The engine supports dynamic connection, disconnection and removal of nodes while running,
+///    with only minor limitations:
+///    - all dynamic reconnections must occur upstream of a mixer
+///    - while removals of effects will normally result in the automatic connection of the adjacent
+///        nodes, removal of a node which has differing input vs. output channel counts, or which
+///        is a mixer, is likely to result in a broken graph.
+///    
+///    By default, the engine is connected to an audio device and automatically renders in realtime.
+///    It can also be configured to operate in manual rendering mode, i.e. not connected to an
+///    audio device and rendering in response to requests from the client, normally at or
+///    faster than realtime rate.
 pub enum AVAudioEngineFFI {}
 
 foreign_obj_type! {
@@ -43,6 +149,14 @@ foreign_obj_type! {
 }
 
 impl AVAudioEngine {
+    ///    @method init
+    ///    @abstract
+    ///        Initialize a new engine.
+    ///
+    ///    On creation, the engine is by default connected to an audio device and automatically renders
+    ///    in realtime. It can be configured to operate in manual rendering mode through
+    ///    `enableManualRenderingMode:format:maximumFrameCount:error:`.
+
     pub fn new() -> Self {
         unsafe {
             let class = class!(AVAudioEngine);
@@ -52,13 +166,64 @@ impl AVAudioEngine {
 }
 
 impl AVAudioEngineRef {
+    ///    @method attachNode:
+    ///    @abstract
+    ///        Take ownership of a new node.
+    ///    @param node
+    ///        The node to be attached to the engine.
+    ///
+    ///    To support the instantiation of arbitrary AVAudioNode subclasses, instances are created
+    ///    externally to the engine, but are not usable until they are attached to the engine via
+    ///    this method. Thus the idiom, without ARC, is:
+    ///
+    ///    ```
+    ///    // when building engine:
+    ///    AVAudioNode *_player;    // member of controller class (for example)
+    ///    ...
+    ///    _player = [[AVAudioPlayerNode alloc] init];
+    ///    [engine attachNode: _player];
+    ///    ...
+    ///    // when destroying engine (without ARC)
+    ///    [_player release];
+    ///    ```
+
     pub fn attach(&self, node: &AVAudioNodeRef) {
         unsafe { msg_send![self, attach: node] }
     }
 
+    ///    @method detachNode:
+    ///    @abstract
+    ///        Detach a node previously attached to the engine.
+    ///
+    ///    If necessary, the engine will safely disconnect the node before detaching it.
+
     pub fn detach(&self, node: &AVAudioNodeRef) {
         unsafe { msg_send![self, detach: node] }
     }
+    ///    @method connect:to:fromBus:toBus:format:
+    ///    @abstract
+    ///        Establish a connection between two nodes.
+    ///    @param node1
+    ///        The source node
+    ///    @param node2
+    ///        The destination node
+    ///    @param bus1
+    ///        The output bus on the source node
+    ///    @param bus2
+    ///        The input bus on the destination node
+    ///    @param format
+    ///        If non-nil, the format of the source node's output bus is set to this
+    ///        format. In all cases, the format of the destination node's input bus is set to
+    ///        match that of the source node's output bus.
+    ///
+    ///    Nodes have input and output buses (AVAudioNodeBus). Use this method to establish
+    ///    one-to-one connections betweeen nodes. Connections made using this method are always
+    ///    one-to-one, never one-to-many or many-to-one.
+    ///
+    ///    Note that any pre-existing connection(s) involving the source's output bus or the
+    ///    destination's input bus will be broken.
+
+    // - (void)connect:(AVAudioNode *)node1 to:(AVAudioNode *)node2 fromBus:(AVAudioNodeBus)bus1 toBus:(AVAudioNodeBus)bus2 format:(AVAudioFormat * __nullable)format;
 
     // pub fn connect(&self, node1: &AVAudioNodeRef, to node2: &AVAudioNodeRef, fromBus bus1: &AudioNodeBusRef, toBus bus2: &AudioNodeBusRef, format: AVAudioFormat?) {
     //    unsafe {
--- src/av_audio_player.rs
+++ src/av_audio_player.rs
@@ -1,12 +1,10 @@
-
 // /*
 //     File:  AVAudioPlayer.h
-    
+
 //     Framework:  AVFoundation
 
 //     Copyright 2008-2016 Apple Inc. All rights reserved.
 // */
-
 // #import <AVFAudio/AVAudioFormat.h>
 // #import <AVFAudio/AVAudioSettings.h>
 // #if __has_include(<AVFAudio/AVAudioSession.h>)
@@ -28,7 +26,6 @@
 // /* For all of these init calls, if a return value of nil is given you can check outError to see what the problem was.
 //  If not nil, then the object is usable for playing
 // */
-
 // /* all data must be in the form of an audio file understood by CoreAudio */
 // - (nullable instancetype)initWithContentsOfURL:(NSURL *)url error:(NSError **)outError;
 // - (nullable instancetype)initWithData:(NSData *)data error:(NSError **)outError;
@@ -58,38 +55,31 @@
 // - (void)stop;
 
 // /* properties */
-
 // @property(readonly, getter=isPlaying) BOOL playing; /* is it playing or not? */
-
 // @property(readonly) NSUInteger numberOfChannels;
 // @property(readonly) NSTimeInterval duration; /* the duration of the sound. */
-
 // /* the UID of the current audio device (as a string) */
 // @property(copy, nullable) NSString *currentDevice API_AVAILABLE(macos(10.13)) API_UNAVAILABLE(ios, watchos, tvos);
 
-// /* the delegate will be sent messages from the AVAudioPlayerDelegate protocol */ 
+// /* the delegate will be sent messages from the AVAudioPlayerDelegate protocol */
 // @property(weak, nullable) id<AVAudioPlayerDelegate> delegate;
 
 // /* one of these properties will be non-nil based on the init... method used */
 // @property(readonly, nullable) NSURL *url; /* returns nil if object was not created with a URL */
 // @property(readonly, nullable) NSData *data; /* returns nil if object was not created with a data object */
-
 // @property float pan API_AVAILABLE(macos(10.7), ios(4.0), watchos(2.0), tvos(9.0)); /* set panning. -1.0 is left, 0.0 is center, 1.0 is right. */
 // @property float volume; /* The volume for the sound. The nominal range is from 0.0 to 1.0. */
 // - (void)setVolume:(float)volume fadeDuration:(NSTimeInterval)duration API_AVAILABLE(macos(10.12), ios(10.0), watchos(3.0), tvos(10.0)); /* fade to a new volume over a duration */
-
 // @property BOOL enableRate API_AVAILABLE(macos(10.8), ios(5.0), watchos(2.0), tvos(9.0)); /* You must set enableRate to YES for the rate property to take effect. You must set this before calling prepareToPlay. */
 // @property float rate API_AVAILABLE(macos(10.8), ios(5.0), watchos(2.0), tvos(9.0)); /* See enableRate. The playback rate for the sound. 1.0 is normal, 0.5 is half speed, 2.0 is double speed. */
-
-
-// /*  If the sound is playing, currentTime is the offset into the sound of the current playback position.  
+// /*  If the sound is playing, currentTime is the offset into the sound of the current playback position.
 // If the sound is not playing, currentTime is the offset into the sound where playing would start. */
 // @property NSTimeInterval currentTime;
 
 // /* returns the current time associated with the output device */
 // @property(readonly) NSTimeInterval deviceCurrentTime API_AVAILABLE(macos(10.7), ios(4.0), watchos(2.0), tvos(9.0));
 
-// /* "numberOfLoops" is the number of times that the sound will return to the beginning upon reaching the end. 
+// /* "numberOfLoops" is the number of times that the sound will return to the beginning upon reaching the end.
 // A value of zero means to play the sound just once.
 // A value of one will result in playing the sound twice, and so on..
 // Any negative number will loop indefinitely until stopped.
@@ -98,30 +88,24 @@
 
 // /* settings */
 // @property(readonly) NSDictionary<NSString *, id> *settings API_AVAILABLE(macos(10.7), ios(4.0), watchos(2.0), tvos(9.0)); /* returns a settings dictionary with keys as described in AVAudioSettings.h */
-
 // /* returns the format of the audio data */
 // @property(readonly) AVAudioFormat *format API_AVAILABLE(macos(10.12), ios(10.0), watchos(3.0), tvos(10.0));
 
 // /* metering */
-
 // @property(getter=isMeteringEnabled) BOOL meteringEnabled; /* turns level metering on or off. default is off. */
-
 // - (void)updateMeters; /* call to refresh meter values */
-
 // - (float)peakPowerForChannel:(NSUInteger)channelNumber; /* returns peak power in decibels for a given channel */
 // - (float)averagePowerForChannel:(NSUInteger)channelNumber; /* returns average power in decibels for a given channel */
-
 // /* The channels property lets you assign the output to play to specific channels as described by AVAudioSession's channels property */
 // /* This property is nil valued until set. */
 // /* The array must have the same number of channels as returned by the numberOfChannels property. */
 // @property(nonatomic, copy, nullable) NSArray<AVAudioSessionChannelDescription *> *channelAssignments API_AVAILABLE(ios(7.0), watchos(2.0), tvos(9.0)) API_UNAVAILABLE(macos) ; /* Array of AVAudioSessionChannelDescription objects */
-
 // @end
 
 // /* A protocol for delegates of AVAudioPlayer */
 // __WATCHOS_AVAILABLE(3_0)
 // @protocol AVAudioPlayerDelegate <NSObject>
-// @optional 
+// @optional
 // /* audioPlayerDidFinishPlaying:successfully: is called when a sound has finished playing. This method is NOT called if the player is stopped due to an interruption. */
 // - (void)audioPlayerDidFinishPlaying:(AVAudioPlayer *)player successfully:(BOOL)flag;
 
@@ -131,7 +115,6 @@
 // #if TARGET_OS_IPHONE
 
 // /* AVAudioPlayer INTERRUPTION NOTIFICATIONS ARE DEPRECATED - Use AVAudioSession instead. */
-
 // /* audioPlayerBeginInterruption: is called when the audio session has been interrupted while the player was playing. The player will have been paused. */
 // - (void)audioPlayerBeginInterruption:(AVAudioPlayer *)player NS_DEPRECATED_IOS(2_2, 8_0);
 
