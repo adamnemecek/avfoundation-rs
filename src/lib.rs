8264f4dae99620b0bf0fada1db9f684df34f1d40
Adam Nemecek <adamnemecek@gmail.com>
2020-11-29 07:41:29 -0800

    added missing files

--- src/lib.rs
+++ src/lib.rs
@@ -135,14 +135,26 @@ macro_rules! foreign_obj_type {
 mod au_audio_unit;
 pub use au_audio_unit::*;
 
+mod au_parameters;
+pub use au_parameters::*;
+
 mod av_audio_buffer;
 pub use av_audio_buffer::*;
 
 mod av_audio_channel_layout;
 pub use av_audio_channel_layout::*;
 
+mod av_audio_connection_point;
+pub use av_audio_connection_point::*;
+
+mod av_audio_converter;
+pub use av_audio_converter::*;
+
 mod av_audio_engine;
 pub use av_audio_engine::*;
+
+mod av_audio_environment_node;
+pub use av_audio_environment_node::*;
 
 mod av_audio_file;
 pub use av_audio_file::*;
@@ -153,20 +165,80 @@ pub use av_audio_format::*;
 mod av_audio_io_node;
 pub use av_audio_io_node::*;
 
+mod av_audio_mixer_node;
+pub use av_audio_mixer_node::*;
+
+mod av_audio_mixing;
+pub use av_audio_mixing::*;
+
 mod av_audio_node;
 pub use av_audio_node::*;
+
+mod av_audio_player_node;
+pub use av_audio_player_node::*;
+
+mod av_audio_player;
+pub use av_audio_player::*;
+
+mod av_audio_recorder;
+pub use av_audio_recorder::*;
+
+mod av_audio_sequencer;
+pub use av_audio_sequencer::*;
 
 mod av_audio_session;
 pub use av_audio_session::*;
 
+mod av_audio_settings;
+pub use av_audio_settings::*;
+
+mod av_audio_sink_node;
+pub use av_audio_sink_node::*;
+
+mod av_audio_source_node;
+pub use av_audio_source_node::*;
+
 mod av_audio_time;
 pub use av_audio_time::*;
 
-mod av_audio_unit_component_manager;
-pub use av_audio_unit_component_manager::*;
+mod av_audio_types;
+pub use av_audio_types::*;
 
 mod av_audio_unit_component;
 pub use av_audio_unit_component::*;
+
+mod av_audio_unit_delay;
+pub use av_audio_unit_delay::*;
+
+mod av_audio_unit_distortion;
+pub use av_audio_unit_distortion::*;
+
+mod av_audio_unit_effect;
+pub use av_audio_unit_effect::*;
+
+mod av_audio_unit_eq;
+pub use av_audio_unit_eq::*;
+
+mod av_audio_unit_generator;
+pub use av_audio_unit_generator::*;
+
+mod av_audio_unit_midi_instrument;
+pub use av_audio_unit_midi_instrument::*;
+
+mod av_audio_unit_reverb;
+pub use av_audio_unit_reverb::*;
+
+mod av_audio_unit_sampler;
+pub use av_audio_unit_sampler::*;
+
+mod av_audio_unit_time_effect;
+pub use av_audio_unit_time_effect::*;
+
+mod av_audio_unit_time_pitch;
+pub use av_audio_unit_time_pitch::*;
+
+mod av_audio_unit_varispeed;
+pub use av_audio_unit_varispeed::*;
 
 mod av_audio_unit;
 pub use av_audio_unit::*;
@@ -174,41 +246,5 @@ pub use av_audio_unit::*;
 mod av_midi_player;
 pub use av_midi_player::*;
 
-mod av_audio_player_node;
-pub use av_audio_player_node::*;
-
-mod av_audio_sequencer;
-pub use av_audio_sequencer::*;
-
-mod au_parameters;
-pub use au_parameters::*;
-
-mod av_audio_types;
-pub use av_audio_types::*;
-
-mod av_audio_sink_node;
-pub use av_audio_sink_node::*;
-
-mod av_audio_settings;
-pub use av_audio_settings::*;
-
-mod av_audio_mixing;
-pub use av_audio_mixing::*;
-
-mod av_audio_mixer_node;
-pub use av_audio_mixer_node::*;
-
-mod av_audio_recorder;
-pub use av_audio_recorder::*;
-
-mod av_audio_source_node;
-pub use av_audio_source_node::*;
-
-mod av_audio_connection_point;
-pub use av_audio_connection_point::*;
-
-mod av_audio_unit_generator;
-pub use av_audio_unit_generator::*;
-
-mod av_audio_unit_midi_instrument;
-pub use av_audio_unit_midi_instrument::*;
+mod av_speech_synthesis;
+pub use av_speech_synthesis::*;
