822126290c9a948bd02ff3df52fd7897197320a0
Adam Nemecek <adamnemecek@gmail.com>
2020-11-29 07:53:49 -0800

    comments for channel_layout + connection_point

--- src/av_audio_connection_point.rs
+++ src/av_audio_connection_point.rs
@@ -3,6 +3,13 @@ use crate::{
     AVAudioNodeRef,
 };
 
+/// @class AVAudioConnectionPoint
+/// @abstract A representation of either a source or destination connection point in AVAudioEngine.
+/// @discussion
+///     AVAudioConnectionPoint describes either a source or destination connection point (node, bus)
+///     in AVAudioEngine's graph.
+///
+///     Instances of this class are immutable.
 pub enum AVAudioConnectionPointFFI {}
 
 foreign_obj_type! {
@@ -12,6 +19,18 @@ foreign_obj_type! {
 }
 
 impl AVAudioConnectionPoint {
+    // - (instancetype)init NS_UNAVAILABLE;
+    pub fn new() -> Self {
+        panic!("unavailable")
+    }
+
+    /// @method initWithNode:bus:
+    /// @abstract Create a connection point object.
+    /// @param node the source or destination node
+    /// @param bus the output or input bus on the node
+    /// @discussion
+    /// 	If the node is nil, this method fails (returns nil).
+    // - (instancetype)initWithNode:(AVAudioNode *)node bus:(AVAudioNodeBus)bus NS_DESIGNATED_INITIALIZER;
     pub fn with_node(node: &AVAudioNodeRef, bus: AVAudioNodeBus) -> Self {
         unsafe {
             let class = class!(AVAudioConnectionPoint);
@@ -22,10 +41,17 @@ impl AVAudioConnectionPoint {
 }
 
 impl AVAudioConnectionPointRef {
+    /// @property node
+    /// @abstract Returns the node in the connection point.
+    /// @property (nonatomic, readonly, weak) AVAudioNode *node;
+
     pub fn node(&self) -> &AVAudioNodeRef {
         unsafe { msg_send![self, node] }
     }
 
+    /// @property bus
+    /// @abstract Returns the bus on the node in the connection point.
+    /// @property (nonatomic, readonly) AVAudioNodeBus bus;
     pub fn bus(&self) -> AVAudioNodeBus {
         unsafe { msg_send![self, bus] }
     }
