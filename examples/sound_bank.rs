3ace95f5291654fd577be264624a6fada4065eb5
adamnemecek <adamnemecek@gmail.com>
2021-01-14 10:26:27 -0800

    cleanup

--- examples/sound_bank.rs
+++ examples/sound_bank.rs
@@ -13,34 +13,28 @@ pub struct Instrument {
 
 impl Instrument {
     pub fn new() -> Self {
-        // println!("start");
         let engine = AVAudioEngine::new();
 
-        // println!("2");
         let sampler = AVAudioUnitSampler::new();
-        // println!("start");
-        // let node = AVAudioNode::new();
-        // let output = engine.output_node();
-        // println!("3");
         engine.attach_node(&sampler);
-        // println!("after attach");
         let output = engine.output_node();
-        // println!("after output");
         engine.connect_nodes(&sampler, output, None);
-        // println!("after connect");
         engine.start_and_return_error();
-        // println!("after start");
         let bank = 121;
         let url = std::path::Path::new("/Users/adamnemecek/Downloads/FatBoy-v0.790.sf2");
 
         // println!("before load");
-        sampler.load_bank_instrument_at_url(url.to_path_buf(), 0, bank, 0);
+        sampler.load_bank_instrument_at_url(url.to_path_buf(), 100, bank, 0);
         println!("after load");
         Self { engine, sampler }
     }
 
-    pub fn play_note(&self, pitch: u8, loudness: u8) {
-        self.sampler.start_note(pitch, loudness, 0)
+    pub fn play_note(&self, note: u8, loudness: u8, channel: u8) {
+        self.sampler.start_note(note, loudness, channel)
+    }
+
+    pub fn stop_note(&self, note: u8, channel: u8) {
+        self.sampler.stop_note(note, channel)
     }
 }
 
@@ -49,10 +43,16 @@ fn main() {
     let instrument = Instrument::new();
     let mut rng = rand::thread_rng();
 
-    for i in 0..100 {
-        std::thread::sleep(std::time::Duration::from_millis(20));
+    for i in 0..20000 {
+        std::thread::sleep(std::time::Duration::from_millis(500));
         let pitch = rng.gen_range(0..127);
-        instrument.play_note(pitch, 100);
+        instrument.play_note(pitch, 100, 0);
+        let pitch = rng.gen_range(0..127);
+        instrument.play_note(pitch, 100, 0);
+        let pitch = rng.gen_range(0..127);
+        instrument.play_note(pitch, 100, 0);
+        let pitch = rng.gen_range(0..127);
+        instrument.play_note(pitch, 100, 0);
     }
     std::thread::sleep(std::time::Duration::from_secs(1));
 }
