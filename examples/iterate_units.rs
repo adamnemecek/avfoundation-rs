86f94f9753a107d7f360231d134ddf810223a942
adamnemecek <adamnemecek@gmail.com>
2021-01-13 18:57:27 -0800

    stuff

--- examples/iterate_units.rs
+++ examples/iterate_units.rs
@@ -8,20 +8,21 @@ fn main() {
     let manager = AVAudioUnitComponentManager::shared();
     // let components = manager.components_passing_test(|unit| (true, ShouldStop::Continue));
     let components = manager.components_passing_test(|unit| {
-        if unit.name().contains("Serum") {
-            (true, ShouldStop::Stop)
-        } else {
-            (false, ShouldStop::Continue)
-        }
+        // if unit.name().contains("Serum") {
+        //     (true, ShouldStop::Stop)
+        // } else {
+        //     (false, ShouldStop::Continue)
+        // }
+        (true, ShouldStop::Continue)
     });
-    let serum = components.first().unwrap();
-    println!("serum {:?}", serum);
+    // let serum = components.first().unwrap();
+    // println!("serum {:?}", serum);
 
-    let desc = serum.audio_component_description();
-    let midi = AVAudioUnitMIDIInstrument::new_with_audio_component_description(desc);
-    println!("serum {:?}", midi);
+    // let desc = serum.audio_component_description();
+    // let midi = AVAudioUnitMIDIInstrument::new_with_audio_component_description(desc);
+    // println!("serum {:?}", midi);
 
-    std::thread::sleep(std::time::Duration::from_secs(1));
+    // std::thread::sleep(std::time::Duration::from_secs(1));
     // for c in components {
     //     println!(
     //         "manufacturer: {:?}, name: {:?}, description: {:?}, type_name {:?}",
