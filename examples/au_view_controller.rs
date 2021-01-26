f3ab7c70d783713c72e78b4ad7f8043c52a0d968
adamnemecek <adamnemecek@gmail.com>
2021-01-26 08:39:36 -0800

    working on loading controller

--- examples/au_view_controller.rs
+++ examples/au_view_controller.rs
@@ -1,1 +1,26 @@
 
+use avfoundation::{
+    AVAudioUnitComponentManager,
+    AVAudioUnitMIDIInstrument,
+    ShouldStop,
+    AVAudioEngine,
+};
+
+fn main() {
+    let manager = AVAudioUnitComponentManager::shared();
+    // let components = manager.components_passing_test(|unit| (true, ShouldStop::Continue));
+    let components = manager.components_passing_test(|unit| {
+        if unit.name().contains("Serum") {
+            (true, ShouldStop::Stop)
+        } else {
+            (false, ShouldStop::Continue)
+        }
+        // (true, ShouldStop::Continue)
+    });
+
+    let desc = components.first().unwrap().audio_component_description();
+
+    let engine = AVAudioEngine::new();
+    // println!("{:?}", components.first());
+    // let midi = AVAudioUnitMIDIInstrument::new_with_audio_component_description(desc);
+}
