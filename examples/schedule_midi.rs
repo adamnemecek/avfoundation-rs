ce56b9288c1599310bbb3321678b2441b66d1eaf
adamnemecek <adamnemecek@gmail.com>
2021-02-11 17:21:00 -0800

    midi_schedule

--- examples/schedule_midi.rs
+++ examples/schedule_midi.rs
@@ -1,6 +1,9 @@
 use avfoundation::prelude::*;
 use kern_return::KERN_SUCCESS;
-use mach::kern_return;
+use mach::{
+    clock_types::NSEC_PER_SEC,
+    kern_return,
+};
 // public func midi_scheduler_main() {
 
 //     let engine = AVAudioEngine()
@@ -46,7 +49,56 @@ impl MachTimebase {
             todo!()
         }
     }
+
+    #[inline]
+    pub fn numer(&self) -> u32 {
+        self.inner.numer
+    }
+
+    #[inline]
+    pub fn denom(&self) -> u32 {
+        self.inner.denom
+    }
 }
+
+// const NSEC_PER_SEC: u32 = 1000000000;
+
+// use mach::
+fn midi_time_range(timestamp: &AudioTimeStamp, frames: f64) -> (f64, f64) {
+    let nsec_per_sec = NSEC_PER_SEC as f64;
+    let sample_rate = 44100.0;
+    let start_time_interval = timestamp.m_sample_time / sample_rate;
+    let duration = frames / sample_rate;
+
+    let midi_start = start_time_interval * nsec_per_sec;
+    let midi_end = midi_start + duration * nsec_per_sec;
+    (midi_start, midi_end)
+}
+
+// NSTimeInterval startTimeInterval = inTimeStamp->mSampleTime / 44100.0;
+// NSTimeInterval duration = inNumberFrames / 44100.0;
+
+// static mach_timebase_info_data_t timebaseInfo = {0,0};
+// if (timebaseInfo.denom == 0) mach_timebase_info(&timebaseInfo);
+// uint64_t midiStartTime = startTimeInterval * NSEC_PER_SEC * timebaseInfo.denom / timebaseInfo.numer;
+// uint64_t midiEndTime = midiStartTime + duration * NSEC_PER_SEC;
+
+// NSMutableArray *commands = [NSMutableArray array];
+// for (MIKMIDICommand *command in self.allNoteMessages) {
+//     if (midiStartTime > command.midiTimestamp) continue;
+//     if (midiEndTime <= command.midiTimestamp) break;
+
+//     [commands addObject:command];
+// }
+
+// if ([self.midiClock musicTimeStampForMIDITimeStamp:midiEndTime] >= self.maxTrackLength)
+//     self->_doneRendering = YES;
+
+// for (MIKMIDINoteOnCommand *command in commands) {
+//     NSLog(@"channel: %i status: %x (%@)", command.channel, (unsigned int)command.statusByte, command.data);
+//     error = MusicDeviceMIDIEvent(self.instrumentUnit, command.statusByte, command.dataByte1, command.dataByte2, 0);
+//     if (error) return error;
+// }
 
 fn main() {
     let engine = AVAudioEngine::new();
@@ -69,6 +121,10 @@ fn main() {
     let notes: Vec<u8> = vec![50, 60, 70, 80, 90];
     let mut i = 0;
     let slice = notes.unsafe_slice();
+
+    let mut running = false;
+    let mut running_ptr: *mut bool = &mut running;
+
     match res {
         Ok(unit) => {
             engine.attach_node(&unit);
@@ -77,12 +133,18 @@ fn main() {
             let midi_fn = unit.au_audio_unit().schedule_midi_fn().unwrap();
             let _token = unit.au_audio_unit().token_by_adding_render_observer_fn(
                 move |flags, ts, frame_count, bus| {
-                    let idx = i % slice.len();
-                    if flags.contains(AudioUnitRenderActionFlags::PreRender) {
-                        let note = slice[idx];
-                        let bytes = [0x90, note, 100];
-                        midi_fn(AUEventSampleTime::immediate(), 0, &bytes);
+                    if !flags.contains(AudioUnitRenderActionFlags::PreRender) {
+                        return;
                     }
+                    let (start, end) = midi_time_range(ts, frame_count as _);
+                    let idx = i % slice.len();
+                    let note = slice[idx];
+                    let bytes = [0x90, note, 100];
+                    midi_fn(AUEventSampleTime::immediate(), 0, &bytes);
+                    unsafe {
+                        *running_ptr = false;
+                    }
+
                     i += 1;
                 },
             );
